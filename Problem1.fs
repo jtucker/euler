670d24ca2d65861c6c848fd4386aaa76ae7a0acb
Jason Tucker <jtucker@gmail.com>
2015-09-28 15:23:34 -0400

    Initial Check-in Completed the 1st problem and started work on the second problem.

---
+++ Problem1.fs
@@ -0,0 +1,13 @@
+﻿// Multiple of 3 and 5 below 1000
+
+let isMultipleOf3or5 item = 
+    if (item % 3 = 0 || item % 5 = 0) then
+     true
+    else
+     false
+
+let sumOfMultiples = [1..999] |> 
+    List.filter isMultipleOf3or5 |>
+    List.sum 
+
+printfn "Sum of multiples below 1000 is %i " sumOfMultiples
---
+++ Problem2.fs
@@ -0,0 +1,13 @@
+﻿
+
+let rec fib x y = 
+    fib x (x + y)
+
+let rec fibFor4mill x y = 
+    let next = x + y
+    if next < 4000000 then
+        fibFor4mill y next
+    else
+        next
+
+printfn "Total: %i" (fibFor4mill 1 2)
