e3fbf00788aba27ea411b736d883a8b864366680
Jason Tucker <jtucker@users.noreply.github.com>
2015-09-30 17:37:52 -0400

    Working version.
    Used the unfold method to generate a sequence from a starting point. Since Seq's are lazily evaluated we don't have to give it a bound when it's created (which is awesome BTW).
    
    Then the rest is just some pipes.

--- Problem2.fs
+++ Problem2.fs
@@ -1,9 +1,7 @@
 
 
-let rec fib x =
-  match x with
-  | 0 -> 0
-  | 1 -> 1
-  | _ -> fib (x - 1) + fib (x - 2)
-
-printfn "output %i" (fib 20)
+let fibs = (0, 1) |> Seq.unfold( fun(a,b) -> Some(a, (b, a+b)) )
+let sumOfFibs = 
+ Seq.takeWhile (fun eI -> eI < 4000000) fibs |>
+ Seq.filter (fun x -> x % 2 = 0 ) |>
+ Seq.sum
